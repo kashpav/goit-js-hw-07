9bbcc457cc58ad97aebaa84810a58ac7b7dba4b2
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-08 20:57:46 +0300

    лайтбокс для галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+console.log(galleryItemsMarkup);
+
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforebegin", galleryItemsMarkup);
