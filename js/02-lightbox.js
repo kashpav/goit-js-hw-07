3def9c31505b24603a109fb790fe419543d61835
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-15 22:20:13 +0300

    розмітка завдання 2 дз 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const render = () => {
+  const galleryItemsMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+
+  console.log(galleryItemsMarkup);
+  galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+};
+
+render();
