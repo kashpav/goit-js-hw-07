fee1cf7700c6975d57e84f7bf05779b236d006d7
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-15 21:46:16 +0300

    виконує першу частину 7 дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 console.log(galleryItems);
 
 const galleryList = document.querySelector(".gallery");
 
-const galleryItemsMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
+const render = () => {
+  const galleryItemsMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -16,17 +18,89 @@ const galleryItemsMarkup = galleryItems
     />
   </a>
 </div>`;
-  })
-  .join("");
+    })
+    .join("");
 
-console.log(galleryItemsMarkup);
-galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
-galleryList.addEventListener("click", onGallertItemsClick);
+  console.log(galleryItemsMarkup);
+  galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+};
 
-const onGallertItemsClick = (e) => {
-  e.prventDefault();
-  const instance = basicLightbox.create(
-    `< img source="${e.target.dataset.source}" />`
+render();
+
+let instance;
+
+function onModalClose() {
+  instance.close();
+}
+
+function onEscPress() {
+  if (event.code === "Escape") {
+    onModalClose();
+  }
+}
+
+const onImageClick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  const currentImg = galleryItems.find(
+    (item) => e.target.alt === item.description
+  );
+  instance = basicLightbox.create(
+    `
+  <img src ="${currentImg.original}"> `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscPress);
+      },
+    }
   );
   instance.show();
 };
+
+galleryList.addEventListener("click", onImageClick);
+
+// ==================================
+// const onImageClick = (e) => {
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   e.preventDefault();
+//   const currentImg = galleryItems.find(
+//     (item) => e.target.alt === item.description
+//   );
+//   // const originalSizeSrc = currentImg.original;
+
+//   const instance = basicLightbox.create(`
+//   <img src ="${currentImg.original}"> `);
+//   instance.show();
+// };
+
+// galleryList.addEventListener("click", onImageClick);
+// ========================================
+// galleryList.addEventListener("click", onGallertItemsClick);
+
+// const onGallertItemsClick = (e) => {
+// e.prventDefault();
+//   const instance = basicLightbox.create(
+//     // `< img src="${e.target.dataset.source}" />`
+//     `<h2>hyhyh</h2>`
+//   );
+//   instance.show();
+// };
+
+//   const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `);
+
+//   instance.show();
+// };
